25359f7a056897d3961fb4a5404714aaa9dc4111
Hakan Çakmak <hakan.cakmak@live.com>
2023-06-21 21:44:49 +0300

    v-0.2

--- js/main.js
+++ js/main.js
@@ -61,9 +61,9 @@ $('#countdown').countdown({
 	  var el = $(this.el);
 	  el.empty()
 	    //.append("<div>" + this.leadingZeros(data.years, 4) + "<span>years</span></div>")
-	    .append("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div>")
-	    .append("<div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div>")
-	    .append("<div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div>")
-	    .append("<div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+	    .append("<div>" + this.leadingZeros(data.days, 2) + " <span>gün</span></div>")
+	    .append("<div>" + this.leadingZeros(data.hours, 2) + " <span>saat</span></div>")
+	    .append("<div>" + this.leadingZeros(data.min, 2) + " <span>dakika</span></div>")
+	    .append("<div>" + this.leadingZeros(data.sec, 2) + " <span>saniye</span></div>");
 	}
 });
